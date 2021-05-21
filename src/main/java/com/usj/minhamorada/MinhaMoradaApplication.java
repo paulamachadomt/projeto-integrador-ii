2dabdda322f2e86a1573d16c1f17d5c2d1d49690
paulamachadomt <paulamachadomt@gmail.com>
2021-05-20 22:46:03 -0300

    start api spring

---
+++ src/main/java/com/usj/minhamorada/MinhaMoradaApplication.java
@@ -0,0 +1,13 @@
+package com.usj.minhamorada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaMoradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaMoradaApplication.class, args);
+	}
+
+}
