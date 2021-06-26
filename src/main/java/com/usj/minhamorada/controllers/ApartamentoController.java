3b4164741269b087c1a29bb4bd36cc5ad69c3966
paulamachadomt <paulamachadomt@gmail.com>
2021-06-26 12:13:31 -0300

    adicionando CrossOrigin nos controllers

--- src/main/java/com/usj/minhamorada/controllers/ApartamentoController.java
+++ src/main/java/com/usj/minhamorada/controllers/ApartamentoController.java
@@ -1,6 +1,7 @@
 package com.usj.minhamorada.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,21 +21,25 @@ public class ApartamentoController {
 		@Autowired
 		ApartamentoService apartamentoService;
 		
+		@CrossOrigin
 		@PostMapping
 		public DTO create(@RequestBody DTO requestDTO) {
 			return apartamentoService.cadastrarApartamento(requestDTO);
 		}
 		
+		@CrossOrigin
 		@GetMapping(value="/{id}")
 		public DTO read(@PathVariable Long id) throws Exception {
 			return apartamentoService.carregarDadosApartamento(id);			
 		}
 		
+		@CrossOrigin
 		@PutMapping(value="/{id}")
 		public DTO update(@PathVariable Long id, @RequestBody DTO requestDTO) {
 			return apartamentoService.atualizarDadosApartamento(id, requestDTO);
 		}
-			
+		
+		@CrossOrigin
 		@DeleteMapping(value="/{id}")
 		public DTO delete(@PathVariable Long id) throws Exception {
 			return apartamentoService.deletarApartamento(id);
--- src/main/java/com/usj/minhamorada/controllers/MoradorController.java
+++ src/main/java/com/usj/minhamorada/controllers/MoradorController.java
@@ -3,6 +3,7 @@ package com.usj.minhamorada.controllers;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,26 +23,31 @@ public class MoradorController {
 	@Autowired
 	MoradorService moradorService;
 	
+	@CrossOrigin
 	@PostMapping
 	public DTO create(@RequestBody DTO request) {
 		return moradorService.cadastrarMorador(request);
 	}
 	
+	@CrossOrigin
 	@GetMapping(value="/{id}")
 	public DTO read(@PathVariable Long id) throws Exception {
 		return moradorService.carregarDadosMorador(id);
 	}
 	
+	@CrossOrigin
 	@PutMapping(value="/{id}")
 	public DTO update(@PathVariable Long id, @RequestBody DTO request) throws Exception {
 		return moradorService.atualizarDadosMorador(id, request);
 	}
 	
+	@CrossOrigin
 	@DeleteMapping(value="/{id}")
 	public DTO delete(@PathVariable Long id) throws Exception {
 		return moradorService.deletarMorador(id);	
 	}
 	
+	@CrossOrigin
 	@GetMapping(value="/")
 	public DTO findAll() throws Exception {
 		return moradorService.listarMoradores();
